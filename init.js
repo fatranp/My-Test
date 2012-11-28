99e9e0b802e0f792d3c54543968e03a46e5a8c27
fatra <putra.fatra08@gmail.com>
2012-11-28 09:37:29 +0700

    tugas IMK
    Argho,Fatra,Umam

---
+++ init.js
@@ -0,0 +1,45 @@
+﻿var init = { 
+     'questions': [ 
+       {
+           'question': 'Ciri-ciri jaringan komputer adalah sebagai berikut ini, <i>kecuali…</i>.',
+           'answers': ['Berbagi pakai perangkat keras','Berbagi pakai perangkat lunak','Berbagi user','Berbagi saluran komunikasi'],
+			  'correctAnswer': 3
+       },
+        {
+           'question': 'Salah satu tipe jaringan komputer yang umum dijumpai adalah….',
+           'answers': ['Star','Bus','Wan','Client-server'],
+			  'correctAnswer': 4
+       },
+       {
+           'question': 'Jenis topologi jaringan <i>kecuali</i>',
+           'answers': ['Star','Bus','Three','Ring'],
+			  'correctAnswer': 3
+       },
+		 {
+           'question': 'IP adress 126.46.5.6 termasuk IP address kelas...',
+           'answers': ['A','B','C','D'],
+			  'correctAnswer': 1
+       },
+       {
+           'question': 'Tipe file yang digunakan linux secara umum adalah ...',
+           'answers': ['INF','FAT','NTFS','EXT3'],
+			  'correctAnswer': 4
+       },
+       {
+           'question': 'Pembagian ruang dalam sebuah hardisk diistilahkan dengan ….',
+           'answers': ['Divisi','Partisi','Kernel','File System'],
+			  'correctAnswer': 2
+       },
+     ],
+	  'resultComments' :  
+	  {
+		    perfect: 'Perfect,istimewa gan',
+			 excellent: 'Excelent, hampir sempurna..',
+			 good: 'Mantap!',
+			 average: 'Lumayan, untuk pemula.',
+			 bad: 'Jelek hasilnya.',
+			 poor: 'Jelek banget..',
+			 worst: 'gag lulus'
+	  }
+
+ };
